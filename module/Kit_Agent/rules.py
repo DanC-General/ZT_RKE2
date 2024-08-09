0f6b2fae9e305d8899b9c018aef41569eac033bb
Daniel Cheng <23126543@student.uwa.edu.au>
2024-08-09 11:05:33 +0800

    Service class transition seems to work

--- module/Kit_Agent/rules.py
+++ module/Kit_Agent/rules.py
@@ -16,16 +16,16 @@ sys.path.append("../syscall-monitor")
 import msg_handler as rq
 # sport:Connection -  should ocassionally wipe
 s = sched.scheduler(time,sleep)
-conn_dict = dict()
+# conn_dict = dict()
 svc_dict = dict()
-stat_dict = dict()
-ml_dict = dict()
+# stat_dict = dict()
+# ml_dict = dict()
 pod_cidr = ""
 msg_q = queue.Queue()
 # Need to split by deployment
-subj_sysc_map = dict()
-prev_subj = PrioQ()
-terminated = dict()
+# subj_sysc_map = dict()
+# prev_subj = PrioQ()
+# terminated = dict()
 
 def parse_conntrack(conn_str,packet):
 # tcp      6 82684 ESTABLISHED src=192.168.122.10 dst=10.43.238.254 sport=51682 dport=8003 src=10.42.0.62 dst=10.1.1.243 sport=22 dport=59424 [ASSURED] mark=0 use=1
@@ -65,46 +65,47 @@ def get_connection(pack):
     return parse_conntrack(output,pack)
 
 def terminate_connection(ip,port):
-    # print(host_det)
+    # Could change this script to only search the namespaces of the relevant services.
     print("Terminating connection on " , ip , " <-> " , port)
     output = subprocess.run(["../scripts/terminate.sh"]+[ip,port])
 
-def handle_alert(syscall,alert_ts,log): 
-    global subj_sysc_map
-    # print("Recieved an alert!!")
-    # print(item)
-    log.write(syscall + str(alert_ts) + "\n")
-    recency = 5
-    # Change to use alert ts
-    for subject in prev_subj.more_recent(alert_ts): 
-        print("Adding to malicious", subject)
-        # if subject not in subj_sysc_map: 
-        #     subj_sysc_map[subject] = dict()
-        # else: 
-        #     # if "total" not in subj_sysc_map[subject]: 
-        #     #     subj_sysc_map[subject]["total"] = 1 * recency
-        subj_sysc_map[subject]["total"] += 1 * recency
-        if syscall not in subj_sysc_map[subject]:
-            subj_sysc_map[subject][syscall] = 1 * recency
-        else:
-            subj_sysc_map[subject][syscall] += 1 * recency
-        # Update so next subject is less recent
-        recency -= 2
-    log.write(str(subj_sysc_map) + "\n")
+# def handle_alert(syscall,alert_ts,log): 
+#     global subj_sysc_map
+#     # print("Recieved an alert!!")
+#     # print(item)
+#     log.write(syscall + str(alert_ts) + "\n")
+#     recency = 5
+#     # Change to use alert ts
+#     for subject in prev_subj.more_recent(alert_ts): 
+#         print("Adding to malicious", subject)
+#         # if subject not in subj_sysc_map: 
+#         #     subj_sysc_map[subject] = dict()
+#         # else: 
+#         #     # if "total" not in subj_sysc_map[subject]: 
+#         #     #     subj_sysc_map[subject]["total"] = 1 * recency
+#         subj_sysc_map[subject]["total"] += 1 * recency
+#         if syscall not in subj_sysc_map[subject]:
+#             subj_sysc_map[subject][syscall] = 1 * recency
+#         else:
+#             subj_sysc_map[subject][syscall] += 1 * recency
+#         # Update so next subject is less recent
+#         recency -= 2
+#     log.write(str(subj_sysc_map) + "\n")
 
 
-def total_abnormal(): 
-    total = 0
-    for subject in subj_sysc_map: 
-        if "total" in subj_sysc_map[subject]:
-            total += subj_sysc_map[subject]["total"]
-    return total
+# def total_abnormal(): 
+#     total = 0
+#     for subject in subj_sysc_map: 
+#         if "total" in subj_sysc_map[subject]:
+#             total += subj_sysc_map[subject]["total"]
+#     return total
 
 def get_lines(pipe): 
-    global conn_dict
-    global stat_dict
-    global terminated
-    global subj_sysc_map
+    global svc_dict
+    # global conn_dict
+    # global stat_dict
+    # global terminated
+    # global subj_sysc_map
     # Check packet counts 
     with open(pipe, 'r') as f: 
         print("looping")
@@ -130,30 +131,36 @@ def get_lines(pipe):
             get_connection(pack)
             log.write(str(pack) + "\n")
 
-            # Update stored statistics
-            stats = stat_dict[pack.svc]
-            # log.write(pack)
-            stats.enqueue(pack) 
 
+            cur_svc = (svc_dict[pack.svc])
+            # stats = stat_dict[pack.svc]
+            # log.write(pack)
+            # stats.enqueue(pack) 
+            # Update stored statistics
+            cur_svc.stats.enqueue(pack)
+
+            # Train relevant ML instance
+            cur_svc.ml.FE.packets.append(pack)
+            rmse =  cur_svc.ml.proc_next_packet()
+            log.write("RMSE for " + pack.svc + str(cur_svc.ml.FE.curPacketIndx) +  ":" + str(rmse) +"\n")
+            
             # Add subject to list of recent for tagging
             subject = pack.external_port(pod_cidr)[0]
             print("Current subject is ", subject)
-            if subject not in subj_sysc_map: 
-                subj_sysc_map[subject] = dict()
-                print("Map did not contain ", subject)
-            if "total" not in subj_sysc_map[subject]: 
-                subj_sysc_map[subject]["total"] = 0
-                print(subject, "did not contain total")
+            ### Move to Service Function
+            # cur_sysc_map = cur_svc.subj_sysc_map
+            # if subject not in cur_sysc_map: 
+            #     cur_sysc_map[subject] = dict()
+            #     print("Map did not contain ", subject)
+            # if "total" not in cur_sysc_map[subject]: 
+            #     cur_sysc_map[subject]["total"] = 0
+            #     print(subject, "did not contain total")
 
-            if not prev_subj.contains(subject):
-                print("APPENDING ", subject, "\n")
-                prev_subj.add((pack.ts,subject))
-                print(prev_subj)
-
-            # Train relevant ML instance
-            ml_dict[pack.svc].FE.packets.append(pack)
-            rmse =  ml_dict[pack.svc].proc_next_packet()
-            log.write("RMSE for " + pack.svc + str(ml_dict[pack.svc].FE.curPacketIndx) +  ":" + str(rmse) +"\n")
+            # if not cur_svc.prev_subj.contains(subject):
+            #     print("APPENDING ", subject, "\n")
+            #     cur_svc.prev_subj.add((pack.ts,subject))
+            #     print(cur_svc.prev_subj)
+            cur_svc.add_recent(subject,pack.ts)
 
             # Extract message from queue if one exists
             #   - otherwise pass.
@@ -164,43 +171,46 @@ def get_lines(pipe):
                 print(item)
                 cur_call = item[0]
                 alert_time = item[1]
-                handle_alert(cur_call,alert_time,log)
+                cur_svc.handle_alert(cur_call,alert_time,log)
             # Evaluate system trust
             obj_trust = rmse
-            subj_trust = -1
-            total_ab_sys = total_abnormal()
-            if subj_sysc_map[subject]["total"] > 0 and total_ab_sys > 0 :
-                print("Set subject trust to ", subj_sysc_map[subject]["total"],"/",total_ab_sys," = ",int(subj_sysc_map[subject]["total"])/int(total_ab_sys))
-                subj_trust = int(subj_sysc_map[subject]["total"])/int(total_ab_sys)
-                log.write("Object trust: " + str(obj_trust) + ". Subject trust for " + subject + ": " + str(subj_trust) + "\n")
-                if cur_call in subj_sysc_map: 
-                    if subj_sysc_map[subject][cur_call] != 1: 
-                        print("Repeat syscall ",cur_call, ", skipping.")
-                        subj_trust = -1
-            else: 
-                log.write("Object trust: " + str(obj_trust) + ". Subject fully trusted\n")
-            subj_sysc_map[subject]["trust"] = subj_trust
-            log.write(str(subj_sysc_map))
+            subj_trust = cur_svc.subject_trust(subject,cur_call,log)
 
-            # Act on system trust
+            ##### MMOVE TO Service FUNCTION 
+            # total_ab_sys = cur_svc.total_abnormal()
+            # if cur_sysc_map[subject]["total"] > 0 and total_ab_sys > 0 :
+            #     print("Set subject trust to ", cur_sysc_map[subject]["total"],"/",total_ab_sys," = ",int(cur_sysc_map[subject]["total"])/int(total_ab_sys))
+            #     subj_trust = int(cur_sysc_map[subject]["total"])/int(total_ab_sys)
+            #     log.write("Object trust: " + str(obj_trust) + ". Subject trust for " + subject + ": " + str(subj_trust) + "\n")
+            #     if cur_call in cur_sysc_map: 
+            #         if cur_sysc_map[subject][cur_call] != 1: 
+            #             print("Repeat syscall ",cur_call, ", skipping.")
+            #             subj_trust = -1
+            # else: 
+            #     log.write("Object trust: " + str(obj_trust) + ". Subject fully trusted\n")
+            # cur_sysc_map[subject]["trust"] = subj_trust
+            # log.write(str(cur_sysc_map))
+
+            # Act on overall request trust
             if obj_trust > 100 or subj_trust > 0.8: 
+                cur_svc.terminate(orig_sip,orig_sport,log)
                 # print("Abnormal RMSE: ",rmse)
-                log.write("ALERTED! Object: " + str(obj_trust) + ".Subject: " + str(subj_trust) + ".\n")
-                if orig_sip not in terminated:
-                    terminated[orig_sip] = dict()
-                if orig_sport not in terminated[orig_sip]: 
-                    log.write("Terminated connection.")
-                    # Dummy value for termination
-                    terminated[orig_sip][orig_sport] = 0
-                    terminate_connection(orig_sip,orig_sport)
+                # log.write("ALERTED! Object: " + str(obj_trust) + ".Subject: " + str(subj_trust) + ".\n")
+                # if orig_sip not in terminated:
+                #     terminated[orig_sip] = dict()
+                # if orig_sport not in terminated[orig_sip]: 
+                #     log.write("Terminated connection.")
+                #     # Dummy value for termination
+                #     terminated[orig_sip][orig_sport] = 0
+                #     terminate_connection(orig_sip,orig_sport)
 
             log.flush()
 
         log.close()
 def make_svcs(): 
     global svc_dict
-    global stat_dict
-    global ml_dict
+    # global stat_dict
+    # global ml_dict
     command1 = ["../scripts/svc_res.sh"]
     p1 = subprocess.Popen(command1, stdout=subprocess.PIPE)
     output = subprocess.check_output(('grep', '^-'), stdin=p1.stdout,universal_newlines=True)
@@ -214,22 +224,91 @@ def make_svcs():
     for x in parsed: 
         arr = x.split(":")
         # print(arr)
-        
-        svc_dict[arr[0]] = arr[1]
-        svc_dict[arr[0]] = Service()
-        stat_dict[arr[0]] = StatTracker()
-        ml_dict[arr[0]] = Kitsune(None,None,maxAE,FMgrace,ADgrace)
+        # svc_dict[arr[0]] = arr[1]
+        svc_dict[arr[0]] = Service(maxAE,FMgrace,ADgrace,arr[0],arr[1])
+        # stat_dict[arr[0]] = StatTracker()
+        # ml_dict[arr[0]] = Kitsune(None,None,maxAE,FMgrace,ADgrace)
     # print(stat_dict)
     # print(svc_dict)
 class Service: 
-    def __init__(self,maxAE,FMgrace,ADgrace): 
+    def __init__(self,maxAE,FMgrace,ADgrace,name,port): 
         self.subj_sysc_map = dict()
         self.prev_subj = PrioQ()
         self.terminated = dict()
-        self.s = sched.scheduler(time,sleep)
         self.ml = Kitsune(None,None,maxAE,FMgrace,ADgrace)
         self.stats = StatTracker()
+        self.name = name
+        self.port = port
+
+    def total_abnormal(self): 
+        total = 0
+        for subject in self.subj_sysc_map: 
+            if "total" in self.subj_sysc_map[subject]:
+                total += self.subj_sysc_map[subject]["total"]
+        return total
     
+    def handle_alert(self,syscall,alert_ts,log): 
+        # print("Recieved an alert!!")
+        # print(item)
+        log.write(syscall + str(alert_ts) + "\n")
+        recency = 5
+        # Change to use alert ts
+        for subject in self.prev_subj.more_recent(alert_ts): 
+            print("Adding to malicious", subject)
+            # if subject not in subj_sysc_map: 
+            #     subj_sysc_map[subject] = dict()
+            # else: 
+            #     # if "total" not in subj_sysc_map[subject]: 
+            #     #     subj_sysc_map[subject]["total"] = 1 * recency
+            self.subj_sysc_map[subject]["total"] += 1 * recency
+            if syscall not in self.subj_sysc_map[subject]:
+                self.subj_sysc_map[subject][syscall] = 1 * recency
+            else:
+                self.subj_sysc_map[subject][syscall] += 1 * recency
+            # Update so next subject is less recent
+            recency -= 2
+        log.write(self.name + ":: " + str(self.subj_sysc_map) + "\n")
+
+    def add_recent(self,subject,time): 
+        cur_sysc_map = self.subj_sysc_map
+        if subject not in cur_sysc_map: 
+            cur_sysc_map[subject] = dict()
+            print("Map did not contain ", subject)
+        if "total" not in cur_sysc_map[subject]: 
+            cur_sysc_map[subject]["total"] = 0
+            print(subject, "did not contain total")
+
+        if not self.prev_subj.contains(subject):
+            print("APPENDING ", subject, "to ",self.name, "\n")
+            self.prev_subj.add((time,subject))
+            print(self.prev_subj)
+        
+    def subject_trust(self,subject,cur_call,log):
+        subj_trust = -1
+        total_ab_sys = self.total_abnormal()
+        smap = self.subj_sysc_map
+        if smap[subject]["total"] > 0 and total_ab_sys > 0 :
+            print("Set subject trust to ", smap[subject]["total"],"/",total_ab_sys," = ",int(smap[subject]["total"])/int(total_ab_sys))
+            subj_trust = int(smap[subject]["total"])/int(total_ab_sys)
+            log.write("Subject trust for " + subject + ": " + str(subj_trust) + "\n")
+            if cur_call in smap: 
+                if smap[subject][cur_call] != 1: 
+                    print("Repeat syscall ",cur_call, ", skipping.")
+                    subj_trust = -1
+        smap[subject]["trust"] = subj_trust
+        log.write(self.name + ":: " + str(self.subj_sysc_map) + "\n")
+        return subj_trust
+    
+    def terminate(self,orig_sip,orig_sport,log):
+        # log.write("ALERTED! Object: " + str(obj_trust) + ".Subject: " + str(subj_trust) + ".\n")
+        if orig_sip not in self.terminated:
+            self.terminated[orig_sip] = dict()
+        if orig_sport not in self.terminated[orig_sip]: 
+            log.write("Terminated connection.")
+            # Dummy value for termination
+            self.terminated[orig_sip][orig_sport] = 0
+            terminate_connection(orig_sip,orig_sport)
+
 def get_cidr(): 
     global pod_cidr
     pod_cidr = subprocess.check_output((
@@ -265,11 +344,14 @@ def retrieve():
     temp.connection.close()
 
 def adjust_trust(): 
-    global subj_sysc_map
-    for subject in subj_sysc_map:
-        subj_sysc_map[subject]["trust"] += 0.5 
-        if subj_sysc_map[subject]["trust"] >= 1:
-            subj_sysc_map[subject]["trust"] = 1
+    # global subj_sysc_map
+    global svc_dict
+    for svc in svc_dict:
+        sysc_map = (svc_dict[svc]).subj_sysc_map
+        for subject in sysc_map:
+            sysc_map[subject]["trust"] += 0.5 
+            if sysc_map[subject]["trust"] >= 1:
+                sysc_map[subject]["trust"] = 1
 
     print("Increased trusts at ", time())
 

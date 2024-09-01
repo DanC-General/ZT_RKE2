ae579a679068b549a56d4a38b91400f26375facf
Daniel Cheng <23126543@student.uwa.edu.au>
2024-09-01 20:52:34 +0800

    Initial results measuring completed - fairly effective metric analysis

--- performance/analyse_log.py
+++ performance/analyse_log.py
@@ -28,14 +28,16 @@ class Attacks:
     def order(self):
         self.all = sorted(self.all, key=lambda x: x.ts)
     def get_closest_atk(self,ts):
+        # print("FINDING CLOSEST ATTACK TO",to_date(ts))
         stored = None
         lowest = None
         for atk in self.all:
-            diff = ts - atk.ts 
-            # print(ts,atk.ts,diff,atk)
-            if diff < 0:
-                continue
+            diff = abs(ts - atk.ts)
+            # print("DIFF for atk",ts,atk.ts,diff,atk)
+            # if diff < 0:
+            #     continue
             if lowest is None: 
+                # print("NEW LOWEST",diff,atk)
                 lowest = diff
                 stored = atk
             if diff < lowest: 
@@ -46,13 +48,14 @@ class Attacks:
         
 
 class Request: 
-    def __init__(self,r_t,o_t,s_t,benign,timestr,pack,term=False): 
+    def __init__(self,r_t,o_t,s_t,benign,alert_ts,timestr,sip,dip,pack,term=False): 
         self.r_trust = r_t 
         self.s_trust = s_t 
         self.o_trust = o_t
+        self.alert_ts = timestr_to_obj(alert_ts)
         self.prev_benign = benign 
         # print("Converting",timestr, datetime.datetime.strptime(timestr,"%d/%m/%Y %H:%M:%S:%f"))
-        self.ts = timestr_to_obj(timestr)
+        self.ts = float(timestr)
         self.terminated = term
         self.last_pack = pack
         self.hosts = set(pack[-2:])
@@ -61,6 +64,12 @@ class Request:
         for k in vars(self): 
             ret += k+"->"+str(vars(self)[k])+" | "
         return ret
+    def make_host_str(self): 
+        host_str = ""
+        for i,v in enumerate(sorted(self.hosts)):
+            # print("Sorted",i,v)
+            host_str += "-" + str(v)
+        return host_str
     
 class Analyser:
     def __init__(self): 
@@ -72,8 +81,10 @@ class Analyser:
         self.start_time = timestr_to_obj(timestr)
     def analyse_line(self,line,f): 
         if re.match(r"[a-zA-Z]+:",line): 
+            return
             print("SYSCALL MAPPING:", line)
         elif re.match(r"(1:)|(\d+\.\d+):",line):
+            return
             print("PACKET DETAILS:",line)
             one = line.split(": ")
             two = one[1].split("|")
@@ -84,25 +95,52 @@ class Analyser:
             dip = two[4].replace("dip->",'').strip()
             self.last_details = [rmse,name,sip,dip]
         elif line.startswith("Request"):
-            print("REQUEST DETAILS",line)
-            details = line.split(" ")
-            req_t = details[3]
-            s_t = re.sub(r'[a-zA-Z]', '', details[6])
-            o_t = re.sub(r'[a-zA-Z]', '', details[9])
-            datestr = details[11] + " " + details[12]
-            print("|",datestr)
+            # print("REQUEST DETAILS",line)
+            # details = line.split(" ")
+            ## Parse request into request, syscalls and packet
+            no_sysc = re.sub(r"[a-zA-Z]+:: {.*}",' ',line)
+            # print("NS",no_sysc)
+            one = no_sysc.split("svc->")
+            # for i,e in enumerate(one): 
+            #     print("ONE",i,e)
+            req_dets = one[0].strip().split(' ')
+            pack_dets = one[1]
+
+            ## Parse packet
+            p_d = pack_dets
+            els = p_d.split("|")
+            # print("p_d",p_d,"els is ",els)
+            # for i,e in enumerate(els): 
+            #     print("ELS",i,e)
+            name = els[0].replace("svc->",'').strip()
+            sip = els[3].replace("sip->",'').strip()
+            dip = els[4].replace("dip->",'').strip()
+            pack_ts = els[7].replace("ts->",'').strip()
+            ## Parse request
+            # print("REQ_DETS",req_dets,"PACK_DETS",pack_dets)
+            # for i,e in enumerate(req_dets): 
+            #     print("REQS",i,e)
+            req_t = req_dets[3]
+            s_t = re.sub(r'[a-zA-Z]', '', req_dets[6])
+            o_t = re.sub(r'[a-zA-Z]', '', req_dets[9])
+            rmse = req_dets[-1].replace(':','')
+            # sip = re.sub(r'[a-zA-Z:]', '', req_dets[])
+            datestr = req_dets[11] + " " + req_dets[12]
+            # print("|",datestr)
             second = f.readline()
-            print("REQ_PACK COUNTS",second)
+            # print("REQ_PACK COUNTS",second)
             ben_c = second.split(" ")[0]
             third = f.readline()
+            self.last_details = [rmse,name,sip,dip]
             if (third.startswith("Terminated")):
-                print("REQUEST TERMINATED",line)
-                self.add_request(Request(req_t,o_t,s_t,ben_c,datestr,self.last_details,True))
+                # print("REQUEST TERMINATED",line)
+                self.add_request(Request(req_t,o_t,s_t,ben_c,datestr,pack_ts,sip,dip,self.last_details,True))
             else: 
-                self.add_request(Request(req_t,o_t,s_t,ben_c,datestr,self.last_details))
+                self.add_request(Request(req_t,o_t,s_t,ben_c,datestr,pack_ts,sip,dip,self.last_details))
                 self.analyse_line(third,f)
         elif re.match(r"\d+ packets processed.",line):
-            print("GENERAL PACKET COUNTS", line)
+            # print("GENERAL PACKET COUNTS", line)
+            pass
         # elif line.startswith("Likelihood"): 
         #     print("LIKELIHOOD",line)
         #     weights = f.readline()
@@ -111,9 +149,14 @@ class Analyser:
         #     print(line)
 
     def add_request(self,req): 
-        print("|",req)
+        # print("|",req)
         self.req_q.append(req)
 
+def to_date(times):
+    if times is not None:
+        # print(type(time))
+        
+        return str(datetime.datetime.fromtimestamp(float(times)).time())
 def main(): 
     parser = ArgumentParser()
     parser.add_argument("file", help="Path of file to anlayse")
@@ -132,6 +175,8 @@ def main():
                 for line in atk_f:
                     # print("ATK LINE",line)
                     if "##" in line: 
+                        if "ORDERED" in line: 
+                            break
                         host = line.replace("##","").strip()
                     try:
                         one = line.split('at')
@@ -149,6 +194,8 @@ def main():
         results.attacks.order()
         for i in results.attacks.all: 
             print("((",i,end=")),  ")
+
+        print("Found",len(results.attacks.all),"attacks")
         # return
         for line in raw: 
             # print(line)
@@ -157,25 +204,57 @@ def main():
         # group_count = 0
         new_group = False
         last_ts = 0
+        count = 0
+        host_map = { "VM1":"10.1.2.5","VM2":"10.1.2.10","LOCAL":"127.0.0.1"}
+        values = {"total":0,"within_90s":0,"correct_host":0}
         for r in results.req_q:
             # print("((",r,end=")),  ")
             # group_count += 1
+            count += 1
             near = results.attacks.get_closest_atk(r.ts)
-            # print(r)
-            # if near[1] not in groups:
-            #     groups[near[1]] = dict()
-            #     groups[near[1]][r.hosts] = list()
-            #     groups[near[1]][r.hosts][0] = 0
-            #     groups[near[1]][r.hosts][1] = r.ts
-            # else: 
-            #     for k,v in groups[near[1]]:
-            #         if set(k) == r.hosts: 
-            #             groups[near[1]][r.hosts][0] += 1
-            #             groups[near[1]][r.hosts][2] = r.ts
-            if r.ts - last_ts > 3: 
-                print("NEW ATTACK GROUP",datetime.datetime.fromtimestamp(r.ts),r.hosts,r.o_trust,r.s_trust)
+            # if count > 3:
+            #     break
+            # Store groups = { hosts -> [ count,start_ts, end_ts ]}
+            found = False
+            host_str = ""
+            for i,v in enumerate(sorted(r.hosts)):
+                # print("Sorted",i,v)
+                host_str += "-" + str(v)
+            if host_str in groups:
+                found = True
+                groups[host_str][0] += 1
+                if r.ts - groups[host_str][2] > 60:
+                    groups[host_str][1] = to_date(groups[host_str][1])
+                    groups[host_str][2] = to_date(groups[host_str][2])
+                    if len(groups[host_str]) > 3:
+                        # try:
+                        #     print(type(groups[host_str][-1]),groups[host_str][-1])
+                        #     groups[host_str][-1] = groups[host_str][-1].time()
+                        # # .date()
+                        # except: 
+                        #     print("In loop" ,groups[host_str][-1])
+                        groups[host_str][-1] = to_date(groups[host_str][-1])
+
+                    print(host_str,"Group ended:",groups[host_str])
+                    values["total"] += 1
+                    if len(groups[host_str]) > 3 and groups[host_str][3] < 90:
+                        print("Alert within 90s of attack:",r.o_trust,"x",r.s_trust,"-->",r.r_trust)
+                        values["within_90s"] += 1
+                        if host_map[groups[host_str][5]] in host_str: 
+                            print("Correct host matched.")
+                            values["correct_host"] += 1
+                    # print("NEW ATTACK GROUP",datetime.datetime.fromtimestamp(r.ts),r.hosts,r.o_trust,r.s_trust,near[0],near[1])
+                    near = results.attacks.get_closest_atk(r.ts)
+                    groups[host_str] = [0,r.ts,r.ts,near[0],near[1].name,near[1].host,near[1].ts]                         
+                else: 
+                    groups[host_str][2] = r.ts 
+            if not found:
+                near = results.attacks.get_closest_atk(r.ts)
+                groups[host_str] = [0,r.ts,r.ts,near[0],near[1].name,near[1].host,near[1].ts]                         
+        print(values)
+            # if r.ts - last_ts > 3: 
+            #     print("NEW ATTACK GROUP",datetime.datetime.fromtimestamp(r.ts),r.hosts,r.o_trust,r.s_trust,near[0],near[1])
             # print("DIFF",r.ts - last_ts)
-            last_ts = r.ts
             # print("CLOSEST ATTACKS TO",r.ts,"==",near[0],near[1])
 
 

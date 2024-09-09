609eec557d4192c7c00a9679d7603555758b322b
Daniel Cheng <23126543@student.uwa.edu.au>
2024-09-09 21:13:11 +0800

    More detailed analysis and value calculations completed

--- performance/analyse_log.py
+++ performance/analyse_log.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 import matplotlib.pyplot as plt
 import datetime
+import math
 import numpy as np
 import os
 import re
@@ -14,7 +15,7 @@ class Attack:
         self.name = name
         self.ts = timestr_to_obj(ts)
         self.host = host
-        self.host_map = { "VM1":"10.1.2.5","VM2":"10.1.2.10","LOCAL":"127.0.0.1"}
+        self.host_map = { "VM1":["10.1.2.5"],"VM2":["10.1.2.10"],"LOCAL":["127.0.0.1","10.1.2.1","10.1.1.241"]}
 
     def get_class(self): 
         if (self.name in ["Brute Force","DoS"]): 
@@ -27,7 +28,7 @@ class Attack:
         for k in vars(self): 
             ret += k+"->"+str(vars(self)[k])+" | "
         return ret
-    def get_ip(self): 
+    def get_ips(self): 
         return self.host_map[self.host]
     
 class Attacks: 
@@ -53,18 +54,29 @@ class Attacks:
         if stored is None: 
             return None, Attack(None,None,None)
         return lowest, stored
+    
     def get_host_atks(self,req):
         stored = None
-        lowest = None
+        lowest = math.inf
+        # print("Evaluating ", req)
         for atk in self.all:
-            diff = abs(req.ts - atk.ts)
-            if lowest is None: 
-                # print("NEW LOWEST",diff,atk)
-                lowest = diff
-                stored = atk
-            if diff < lowest and atk.get_ip() in req.hosts: 
-                lowest = diff 
-                stored = atk
+            diff = req.ts - atk.ts
+            num = 0
+            if diff < lowest and diff > -5 and diff < 300:
+            # if lowest is None: 
+            #     # print("NEW LOWEST",diff,atk)
+            #     lowest = diff
+            #     stored = atk
+                for i in req.hosts:
+                    if i.startswith("10.42"):
+                        num+=2
+                    if i in atk.get_ips(): 
+                        num+= 1
+                if num == 3:
+                    # print("Ips matched",i,req.hosts)
+                    lowest = diff 
+                    stored = atk
+            # if diff < lowest and atk.get_ip() in req.hosts: 
         if stored is None: 
             return None, Attack(None,None,None)
         return lowest, stored
@@ -172,93 +184,186 @@ class Analyser:
         det = [x.strip() for x in line.strip().split(" ") if x.strip() != '']
         # print(line,det)
         if len(det) != 6:
-            print("Illegal line",line,det)
+            # print("Illegal line",line,det)
             return
         ts = det[0].replace("|",'').strip()
         rmse = det[-1]
-        sip = det[1]
-        dip = det[2]
-        
-        last_details = [rmse,"compare",sip,dip]
-        # print("added ",last_details, "from", det)
-        self.add_request(Request(None,rmse,None,None,None,ts,None,last_details,True))
+        if float(rmse) < 0.4: 
+            # print("Excluding",rmse)
+            return
+        else:
+            sip = det[1]
+            dip = det[2]
+            last_details = [rmse,"compare",sip,dip]
+            # print("added ",last_details, "from", det)
+            self.add_request(Request(None,rmse,None,None,None,ts,None,last_details,True))
 
     def add_request(self,req): 
         # print("|",req)
         self.req_q.append(req)
 
+def analyse_comparison(file_name):
+    if not os.path.exists(file_name): 
+        print("Invalid file for comparison.")
+        return
+    results = Analyser()
+    with open(file_name,'r') as f:
+        for line in f:
+            results.analyse_comp_line(line)
+    # print(results.req_q)
+    # all_grps = get_groups_from_analyser(results)
+    return results
+
+def get_group_times(group_list,start_time): 
+    atk_ranges = list()
+    for grp in group_list:
+        # print("Group",grp)
+        start = grp[1] - start_time
+        end = grp[2] -  start_time
+        atk_ranges.append((int(start),int(end)))
+    return atk_ranges
+
+def get_groups_from_analyser(results,atks):
+    groups = dict()
+    count = 0
+    host_no_attack = 0
+    within_90s = 0
+    host_map = { "VM1":"10.1.2.5","VM2":"10.1.2.10","LOCAL":"127.0.0.1"}
+    values = {"total":0,"within_90s":0,"correct_host":0}
+    all_groups = list()
+    host_list = []
+    for r in results.req_q:
+        # print("((",r,end=")),  ")
+        # group_count += 1
+        count += 1
+        near = atks.get_host_atks(r)
+        if near[0] is None: 
+            host_no_attack += 1
+        elif near[0] < 300: 
+            within_90s += 1
+        #     print("Incremented",near[1],r)
+        # else:
+        #     print("Near nonnull",near,near[1])
+        # Store groups = { hosts -> [ count,start_ts, end_ts ]}
+        # print(near)
+        found = False
+        host_str = ""
+        # print("Adding",r.hosts,r.last_pack)
+        for i,v in enumerate(sorted(r.hosts)):
+            # print("Sorted",i,v)
+            # print("adding",v)
+            host_str += "-" + str(v)
+        if host_str not in host_list: 
+            host_list.append(host_str)
+        if host_str in groups:
+            # print(host_str)
+            found = True
+            groups[host_str][0] += 1
+            # If there is a gap larger than 60 seconds between packets, end the current flow.
+            if r.ts - groups[host_str][2] > 60:
+                if len(groups[host_str]) > 4:
+                    groups[host_str][6] = to_date(groups[host_str][6])
+                # groups[host_str].append(r.sysc_str)
+                # if "10.1.2.5" in host_str: 
+                # print(host_str,"Group ended:",groups[host_str])
+                all_groups.append(groups[host_str].copy())
+                values["total"] += 1
+
+                # if groups[host_str][3] is not None and groups[host_str][3] < 90:
+                #     print("Alert within 90s of attack:",r.o_trust,"x",r.s_trust,"-->",r.r_trust)
+                #     values["within_90s"] += 1
+                #     if host_map[groups[host_str][5]] in host_str: 
+                #         print("Correct host matched.")
+                #         values["correct_host"] += 1
+                # print("NEW ATTACK GROUP",datetime.datetime.fromtimestamp(r.ts),r.hosts,r.o_trust,r.s_trust,near[0],near[1])
+                # near = results.attacks.get_host_atks(r)
+                groups[host_str] = [0,r.ts,r.ts,near[0],near[1].name,near[1].host,near[1].ts,r.hosts]                         
+                # groups[host_str] = [0,r.ts,r.ts,near[0],near[1].name,near[1].host,near[1].ts,r.sysc_str]                        
+            else: 
+                groups[host_str][2] = r.ts 
+        if not found:
+            # near = results.attacks.get_host_atks(r)
+            groups[host_str] = [0,r.ts,r.ts,near[0],near[1].name,near[1].host,near[1].ts,r.hosts]      
+    # print(all_groups)
+    print("PARSED ", count, "packets")
+    print("LISTS",host_list)
+    print("COUNTS", count, "UNKNOWN",host_no_attack,"prop unknown",host_no_attack/count,"within90",within_90s,"prop",within_90s/count)
+    return all_groups
+
 def to_date(times):
     if times is not None:
         return str(datetime.datetime.fromtimestamp(float(times)).time()) 
 
-def parse_attack_file(args):
-    print(args.file) 
-    with open(args.file,"r") as raw: 
+def parse_attack_file(file,start):
+    atks = Attacks()
+    if file is not None:
+        print("Attack file given at",file)
+        with open(file,"r") as atk_f: 
+            host="LOCAL"
+            for line in atk_f:
+                # print("ATK LINE",line)
+                if "##" in line: 
+                    if "ORDERED" in line: 
+                        break
+                    host = line.replace("##","").strip()
+                try:
+                    one = line.split('at')
+                    ts = one[1]
+                    two = one[0].split('malicious')
+                    atk = two[1]
+                    host = host 
+                    atk_details = Attack(atk.strip(),ts.strip(),host.strip())
+                    if atk_details.is_newer(start):
+                        # print(atk_details)
+                        atks.add_attack(atk_details)
+                    # print("Parsed ts:",ts.strip(),"atk:",atk.strip(),"host",host)
+                except:
+                    pass
+    else: 
+        return
+    atks.order()
+    # for i in atks.all: 
+    #     print("((",i,end=")),  ")
+    # print("Found",len(atks.all),"attacks")
+    return atks
+    
+def parse_log_file(fname):
+    print(fname) 
+    with open(fname,"r") as raw: 
         results = Analyser()
         line = raw.readline()
         print("|st",timestr_to_obj(line))
-        results.set_start(line)
-        if args.attack_file is not None:
-            print("Attack file given at",args.attack_file )
-            with open(args.attack_file,"r") as atk_f: 
-                host="LOCAL"
-                for line in atk_f:
-                    # print("ATK LINE",line)
-                    if "##" in line: 
-                        if "ORDERED" in line: 
-                            break
-                        host = line.replace("##","").strip()
-                    try:
-                        one = line.split('at')
-                        ts = one[1]
-                        two = one[0].split('malicious')
-                        atk = two[1]
-                        host = host 
-                        atk_details = Attack(atk.strip(),ts.strip(),host.strip())
-                        if atk_details.is_newer(results.start_time):
-                            # print(atk_details)
-                            results.attacks.add_attack(atk_details)
-                        # print("Parsed ts:",ts.strip(),"atk:",atk.strip(),"host",host)
-                    except:
-                        pass
-        else: 
-            return
-        
-        results.attacks.order()
-        for i in results.attacks.all: 
-            print("((",i,end=")),  ")
-        print("Found",len(results.attacks.all),"attacks")
-        # return
+        results.set_start(line)        
         for line in raw: 
             # print(line)
             results.analyse_line(line,raw)
         return results
-    return None
+
 def main(): 
     parser = ArgumentParser()
     parser.add_argument("file", help="Path of file to anlayse")
     parser.add_argument("-a","--attack-file",help="Path of file containing the attack logs")
     args = parser.parse_args()
+    results = parse_log_file(args.file)
+    atks = parse_attack_file(args.attack_file,results.start_time)
 
-    results = parse_attack_file(args)
-
-    all_groups = get_groups_from_analyser(results)
+    all_groups = get_groups_from_analyser(results,atks)
     zt_rke2_group = get_group_times(all_groups,results.start_time)
-    comp_groups = analyse_comparison("../module/Kit_Agent/100k_minimal.log")
+    comp_analyser = analyse_comparison("../module/Kit_Agent/100k_minimal.log")
+    comp_groups = get_groups_from_analyser(comp_analyser,atks)
     kit_group = get_group_times(comp_groups,results.start_time)
-    print("ZT_RKE2 GROUPS")
-    for i in all_groups: 
-        print("(( ",i,end=" ))")
-    print("COMPARISON GROUPS")
-    for i in kit_group: 
-        print("(( ",i,end=" ))")
+    # print("ZT_RKE2 GROUPS")
+    # for i in all_groups: 
+    #     print("(( ",i,end=" ))")
+    # print("COMPARISON GROUPS")
+    # for i in kit_group: 
+    #     print("(( ",i,end=" ))")
     # from 22:15 to 23:05 
     # Create the line plot
     values = range(0, 3600)
     # target_ranges = [(5, 7), (34, 36)]
     ground_truths = dict()
-    for atk in results.attacks.all: 
-        # print(int(atk.ts - results.start_time))
+    for atk in atks.all: 
         ground_truths[int(atk.ts - results.start_time)] = atk.get_class()
     # Create a list to store the corresponding values
     plot_values = []
@@ -268,9 +373,9 @@ def main():
     # print(ground_truths)
     all_atks = []
     comp_vals = []
-    compare_ts = analyse_comparison("../module/Kit_Agent/100k_minimal.log")
-    for i,c_ts in enumerate(compare_ts): 
-        compare_ts[i] = int(float(c_ts[0]) - results.start_time)
+    # compare_ts = analyse_comparison("../module/Kit_Agent/100k_minimal.log")
+    # for i,c_ts in enumerate(compare_ts): 
+    #     compare_ts[i] = int(float(c_ts[0]) - results.start_time)
         # print(c_ts[1])
     # print("Compare",compare_ts)
     for value in values:
@@ -278,6 +383,11 @@ def main():
             plot_values.append(1)
         else:
             plot_values.append(None)
+        if any(start <= value <= end for start, end in kit_group):
+            comp_vals.append(1.25)
+        else:
+            comp_vals.append(None)
+
         if value in ground_truths:
             all_atks.append(0.25)
             if ground_truths[value] == "Network": 
@@ -290,12 +400,12 @@ def main():
             net_atks.append(None)
             all_atks.append(None)
             host_atks.append(None)
-        if value in compare_ts: 
-            comp_vals.append(1.25)
-        else:
-            comp_vals.append(None)
+        # if value in compare_ts: 
+        #     comp_vals.append(1.25)
+        # else:
+        #     comp_vals.append(None)
         # if 
-    print("Values",plot_values)
+    # print("Values",plot_values)
     # print(plot_2_values)
     plt.figure(figsize=(20,10))
     plt.plot(values, plot_values, drawstyle='steps-post',markersize=3,marker='o',label="Detected Attacks")
@@ -313,80 +423,6 @@ def main():
     # plt.savefig(f'out/31_8_{time.strftime("%Y%m%d-%H%M%S")}.png')
     plt.show()
 
-def analyse_comparison(file_name):
-    if not os.path.exists(file_name): 
-        print("Invalid file for comparison.")
-        return
-    results = Analyser()
-    with open(file_name,'r') as f:
-        for line in f:
-            results.analyse_comp_line(line)
-    # print(results.req_q)
-    all_grps = get_groups_from_analyser(results)
-    return all_grps
-
-def get_group_times(group_list,start_time): 
-    atk_ranges = list()
-    for grp in group_list:
-        print("Group",grp)
-        start = grp[1] - start_time
-        end = grp[2] -  start_time
-        # start = grp[1]
-        # end = grp[2]
-        atk_ranges.append((int(start),int(end)))
-    return atk_ranges
-
-def get_groups_from_analyser(results):
-    groups = dict()
-    count = 0
-    host_map = { "VM1":"10.1.2.5","VM2":"10.1.2.10","LOCAL":"127.0.0.1"}
-    values = {"total":0,"within_90s":0,"correct_host":0}
-    all_groups = list()
-    for r in results.req_q:
-        # print("((",r,end=")),  ")
-        # group_count += 1
-        count += 1
-        near = results.attacks.get_host_atks(r)
-        # Store groups = { hosts -> [ count,start_ts, end_ts ]}
-        # print(near)
-        found = False
-        host_str = ""
-        # print("Adding",r.hosts,r.last_pack)
-        for i,v in enumerate(sorted(r.hosts)):
-            # print("Sorted",i,v)
-            # print("adding",v)
-            host_str += "-" + str(v)
-        if host_str in groups:
-            # print(host_str)
-            found = True
-            groups[host_str][0] += 1
-            # If there is a gap larger than 60 seconds between packets, end the current flow.
-            if r.ts - groups[host_str][2] > 60:
-                if len(groups[host_str]) > 4:
-                    groups[host_str][6] = to_date(groups[host_str][6])
-                # groups[host_str].append(r.sysc_str)
-                # if "10.1.2.5" in host_str: 
-                print(host_str,"Group ended:",groups[host_str])
-                all_groups.append(groups[host_str].copy())
-                values["total"] += 1
-
-                if groups[host_str][3] is not None and groups[host_str][3] < 90:
-                    print("Alert within 90s of attack:",r.o_trust,"x",r.s_trust,"-->",r.r_trust)
-                    values["within_90s"] += 1
-                    if host_map[groups[host_str][5]] in host_str: 
-                        print("Correct host matched.")
-                        values["correct_host"] += 1
-                # print("NEW ATTACK GROUP",datetime.datetime.fromtimestamp(r.ts),r.hosts,r.o_trust,r.s_trust,near[0],near[1])
-                near = results.attacks.get_host_atks(r)
-                groups[host_str] = [0,r.ts,r.ts,near[0],near[1].name,near[1].host,near[1].ts,r.hosts]                         
-                # groups[host_str] = [0,r.ts,r.ts,near[0],near[1].name,near[1].host,near[1].ts,r.sysc_str]                        
-            else: 
-                groups[host_str][2] = r.ts 
-        if not found:
-            near = results.attacks.get_host_atks(r)
-            groups[host_str] = [0,r.ts,r.ts,near[0],near[1].name,near[1].host,near[1].ts,r.hosts]      
-    # print(all_groups)
-    return all_groups
 
 if __name__ == "__main__": 
     # analyse_comparison("../module/Kit_Agent/50k_minimal.log")

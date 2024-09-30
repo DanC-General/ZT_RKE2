a6fa5ead35068de1d3c5551e705ee2718b7457a9
Daniel Cheng <23126543@student.uwa.edu.au>
2024-09-30 19:48:35 +0800

    Updated trust component evaluation and added roles

--- performance/host_proportion_detection.py
+++ performance/host_proportion_detection.py
@@ -13,6 +13,11 @@ host_count = 0
 net_count = 0
 total_count = 0
 all_results = []
+fp_results = []
+fp_count = 0
+both_fp = 0
+host_fp = 0
+net_fp = 0
 with open(args.file,'r') as f: 
     for line in f: 
         if line.startswith("Attack file"):
@@ -20,11 +25,19 @@ with open(args.file,'r') as f:
             if host_only_count != 0:
             # if total_count != 0: 
                 # print(host_only_count,host_count,total_count,host_only_count/total_count,host_count/total_count)
+                print("Adding host",[host_only_count,host_count,total_count,host_only_count/total_count,host_count/total_count])
                 all_results.append([host_only_count,host_count,total_count,host_only_count/total_count,host_count/total_count])
+            if fp_count != 0: 
+                print("Adding",[host_fp,net_fp,both_fp,fp_count])
+                fp_results.append([host_fp,net_fp,both_fp,fp_count])
             host_only_count = 0
             host_count = 0
             net_count = 0
             total_count = 0
+            fp_count = 0 
+            host_fp = 0
+            net_fp = 0
+            both_fp = 0
         line = line.strip()
         det = line.split(",")
         if det[-1] == "HOST":
@@ -35,14 +48,29 @@ with open(args.file,'r') as f:
                 host_count += 1
                 
         # FP
-        # if det[-1] == "FP":
-        #     total_count += 1
-        #     if float(det[-2]) > 0.4:
-        #         net_count += 1 
-        #     if float(det[-3]) < 1: 
-        #         host_count += 1
+        if det[-1] == "FP":
+            fp_count += 1
+            if float(det[-2]) > 0.2 and float(det[-3]) < 0.7:
+                both_fp += 1
+            elif float(det[-2]) > 0.4:
+                net_fp += 1 
+            # elif float(det[-3]) < 0.5: 
+            else:
+                host_fp += 1
     # print(host_only_count,host_count,total_count,host_only_count/total_count,host_count/total_count)
     # print(net_count,host_count,total_count,net_count/total_count,host_count/total_count)
-
+    subj_only = np.mean([x[-2] for x in all_results])
+    both = np.mean([x[-1] for x in all_results])
     print("Only subject trust:",np.mean([x[-2] for x in all_results]))
     print("Lowered subject trust:",np.mean([x[-1] for x in all_results]))
+    p_fp_host = np.mean([x[0]/x[-1] for x in fp_results])
+    p_fp_net = np.mean([x[1]/x[-1] for x in fp_results])
+    p_fp_both = np.mean([x[2]/x[-1] for x in fp_results])
+    print("Host FP:",np.mean([x[0]/x[-1] for x in fp_results]))
+    print("Net FP:",np.mean([x[1]/x[-1] for x in fp_results]))
+    print("Combined FP:",np.mean([x[2]/x[-1] for x in fp_results]))
+    df = pd.DataFrame([['Host TP',subj_only,both-subj_only,1-both],['All FP',p_fp_host,p_fp_both,p_fp_net]],columns=["Packet Label","Subject Alert","Both","Object Alert"])
+    df.plot(stacked=True,x="Packet Label",kind='bar',rot=0,title="Subject Role in Host Attack Detection")
+    # df = pd.DataFrame([['Host',subj_only,1-subj_only],['Both',both,1-both]],columns=["Type","Detected","Missed"])
+    # df.plot(stacked=True,x="Type",kind='bar',title="Subject Trust Efficacy")
+    plt.show()
---
+++ performance/new_host_prop.py
@@ -0,0 +1,103 @@
+from argparse import ArgumentParser
+import statistics
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+parser = ArgumentParser()
+parser.add_argument("file",help="Path of file to write to")
+args = parser.parse_args()
+kitsune = [list() for x in range(0,4)]
+ztrke2 = [list() for x in range(0,4)]
+# host_only_count = 0
+# host_count = 0
+# net_count = 0
+# total_count = 0
+# all_results = []
+# fp_results = []
+# fp_count = 0
+# both_fp = 0
+# host_fp = 0
+# net_fp = 0
+class Metric(): 
+    def __init__(self,name): 
+        self.results = []
+        self.name = name
+        self.total_count = 0
+        self.both_count = 0
+        self.host_count = 0
+        self.net_count = 0
+    def reset(self):
+        self.total_count = 0
+        self.both_count = 0
+        self.host_count = 0
+        self.net_count = 0
+    def get_results(self): 
+        # [s for s in self.results: ]
+        arr = list()
+        for i in range(0,3):
+            arr.append((float(np.mean([x[i]/x[-1] for x in self.results]))))
+        return arr
+# Host TP, Net TP, All FP 
+metrics = {"NET": Metric("NET"),"HOST":Metric("HOST"),"FP": Metric("FP")}
+with open(args.file,'r') as f: 
+    for line in f: 
+        if line.startswith("Attack file"):
+            # print(line)
+            for k,m in metrics.items(): 
+                if k == "HOST" or k == "NET":
+                    if m.host_count != 0: 
+                        m.results.append([m.host_count,m.both_count,m.net_count,m.total_count])
+                elif m.total_count != 0:
+                    m.results.append([m.host_count,m.both_count,m.net_count,m.total_count])
+                m.reset()
+        line = line.strip()
+        det = line.split(",")
+        if det[-1] == "NET" or det[-1] == "HOST":
+            metrics[det[-1]].total_count += 1
+            add = False
+            if float(det[-2]) < 0.4:
+                metrics[det[-1]].host_count += 1 
+                add = True
+            if float(det[-3]) != 1: 
+                metrics[det[-1]].both_count += 1
+                add = True
+            if not add: 
+                metrics[det[-1]].net_count += 1 
+
+                
+        # FP
+        if det[-1] == "FP":
+            metrics["FP"].total_count += 1
+            if float(det[-2]) > 0.2 and float(det[-3]) < 0.7:
+                metrics["FP"].both_count += 1
+            elif float(det[-2]) > 0.4:
+                metrics["FP"].net_count += 1 
+            # elif float(det[-3]) < 0.5: 
+            else:
+                metrics["FP"].host_count += 1
+    # print(host_only_count,host_count,total_count,host_only_count/total_count,host_count/total_count)
+    # print(net_count,host_count,total_count,net_count/total_count,host_count/total_count)
+    print([[k,v.get_results()] for k,v in metrics.items()])
+    # subj_only = np.mean([x[-2] for x in all_results])
+    # both = np.mean([x[-1] for x in all_results])
+    # print("Only subject trust:",np.mean([x[-2] for x in all_results]))
+    # print("Lowered subject trust:",np.mean([x[-1] for x in all_results]))
+    # p_fp_host = np.mean([x[0]/x[-1] for x in fp_results])
+    # p_fp_net = np.mean([x[1]/x[-1] for x in fp_results])
+    # p_fp_both = np.mean([x[2]/x[-1] for x in fp_results])
+    # print("Host FP:",np.mean([x[0]/x[-1] for x in fp_results]))
+    # print("Net FP:",np.mean([x[1]/x[-1] for x in fp_results]))
+    # print("Combined FP:",np.mean([x[2]/x[-1] for x in fp_results]))
+    hres = metrics["HOST"].get_results()
+    nres = metrics["NET"].get_results()
+    fpres = metrics["FP"].get_results()
+    df = pd.DataFrame([
+                        ['Host TP',hres[0],hres[1]-hres[0],1-hres[1]],
+                        ["Net TP",nres[0],nres[1]-nres[0],1-nres[1]],
+                        ['All FP',fpres[0],fpres[1]-fpres[0],1-fpres[1]]
+                      ],
+                      columns=["Packet Label","Subject Alert","Both","Object Alert"]
+    )
+    df.plot(stacked=True,x="Packet Label",kind='bar',rot=0,title="Trust Component Roles in Attack Detection")
+    plt.show()
+    plt.savefig("Trust_Roles.png")
